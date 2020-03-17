a5c9b6d04218fe136d6b4c61d680dbe7f0c16cf4
ddhorstman <davidzilla27@gmail.com>
2020-03-16 20:40:46 -0400

    Done!

--- src/components/UserCardContainer.js
+++ src/components/UserCardContainer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import UserCard from "./UserCard";
-import {CardDeck}from "reactstrap";
+import { CardDeck } from "reactstrap";
 class UserCardContainer extends React.Component {
     state = {
         cardInfo: "",
@@ -12,18 +12,22 @@ class UserCardContainer extends React.Component {
             .then(res => this.setState({ cardInfo: res }));
         fetch("https://api.github.com/users/ddhorstman/followers")
             .then(res => res.json())
-            .then(res => res.map(entry => fetch(entry.url)
-                .then(res => res.json())
-                //.then(r => { console.log(r); return r; })
-                .then(res => this.setState({ followerInfo: [...this.state.followerInfo, res] }))
+            .then(res => res.map(
+                entry => fetch(entry.url)
+                    .then(res => res.json())
+                    //.then(r => { console.log(r); return r; })
+                    .then(res => this.setState({ followerInfo: [...this.state.followerInfo, res] }))
             ));
     };
     render() {
-        return <CardDeck>
-            <UserCard {...this.state.cardInfo} />
-            {this.state.followerInfo.map((entry, idx) =>
-                <UserCard {...entry} key={idx} />)}
-        </CardDeck>;
+        return (<>
+            <h1>Github User Cards:</h1>
+            <CardDeck>
+                <UserCard {...this.state.cardInfo} />
+                {this.state.followerInfo.map((entry, idx) =>
+                    <UserCard {...entry} key={idx} />)}
+            </CardDeck>
+        </>);
     }
 }
 export default UserCardContainer;
