4504e313555ba2aa8e6020b7722c825a45382a52
ddhorstman <davidzilla27@gmail.com>
2020-03-16 20:31:23 -0400

    MVP functionality!

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,4 +1,11 @@
 import React from "react";
-export default function UserCard(props){
-    return <div />;
+export default function UserCard(props) {
+    return props.login
+        ? (<div>
+            <h2>{props.name}</h2>
+            <img alt={props.login+" image"} src={props.avatar_url} /><br/>
+            <a href={props.html_url}>{props.login}</a>
+            {props.location && <p>Location: {props.location}</p>}
+        </div>)
+        : <div />;
 }
--- src/components/UserCardContainer.js
+++ src/components/UserCardContainer.js
@@ -1,8 +1,29 @@
 import React from "react";
-class UserCardContainer extends React.Component{
-
-    render(){
-        return <div />;
+import UserCard from "./UserCard";
+class UserCardContainer extends React.Component {
+    state = {
+        cardInfo: "",
+        followerInfo: [],
+    };
+    componentDidMount = () => {
+        fetch("https://api.github.com/users/ddhorstman")
+            .then(res => res.json())
+            .then(res => this.setState({ cardInfo: res }));
+        fetch("https://api.github.com/users/ddhorstman/followers")
+            .then(res => res.json())
+            .then(res => res.map(entry => fetch(entry.url)
+                .then(res => res.json())
+                //.then(r => { console.log(r); return r; })
+                .then(res => this.setState({ followerInfo: [...this.state.followerInfo, res] }))
+            ));
+    };
+    render() {
+        return <>
+            <UserCard {...this.state.cardInfo} />
+            <h1>Followers:</h1>
+            {this.state.followerInfo.map((entry, idx) =>
+                <UserCard {...entry} key={idx} />)}
+        </>;
     }
 }
 export default UserCardContainer;
